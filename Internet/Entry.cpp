4d3ee634357d9e1063bb4b6e1a522ad30b66ee57
ConfederateCat <shmurkioskull@gmail.com>
2025-01-08 12:20:23 -0500

    ntpv3 struct

--- Internet/Entry.cpp
+++ Internet/Entry.cpp
@@ -1,73 +1,8 @@
 #include "Internet.hpp"
 
-
 int main(void)
 {
-	//
-	// Create and zero out the NTP packet.
-	//
-	std::uint8_t NtpPacket[NTP_PACKET_SIZE] = { 0 };
-	NtpPacket[0] = NTP_HEADER_BYTE;
+	PNTP_3_HEADER NtpPacket = reinterpret_cast<PNTP_3_HEADER>(calloc(1, sizeof(NTP_3_HEADER)));
 
-	//
-	// Configure the server address.
-	//
-	sockaddr_in ServerAddress{};
-	ServerAddress.sin_family = AF_INET;
-	ServerAddress.sin_port = htons(NTP_PORT);
-
-	if (inet_pton(AF_INET, "216.239.35.0", &ServerAddress.sin_addr) <= 0)
-	{
-		std::cerr << "inet_pton failed" << std::endl;
-		return 1;
-	}
-
-	//
-	// Create the UDP socket.
-	//
-	std::int32_t SockFd = socket(AF_INET, SOCK_DGRAM, 0);
-
-	if (SockFd < 0)
-	{
-		std::cerr << "Socket creation failed" << std::endl;
-		return 1;
-	}
-
-	ssize_t Sent = sendto(SockFd, NtpPacket, NTP_PACKET_SIZE, 0, reinterpret_cast<sockaddr*>(&ServerAddress), sizeof(ServerAddress));
-
-	if (Sent < 0)
-	{
-		std::cerr << "sendto failed" << std::endl;
-		close(SockFd);
-		return 1;
-	}
-
-	//
-	// Receive the NTP response packet.
-	//
-	sockaddr_in ReceiveAddress{};
-	socklen_t AddressLength = sizeof(ReceiveAddress);
-	ssize_t Received = recvfrom(SockFd, NtpPacket, NTP_PACKET_SIZE, 0, reinterpret_cast<sockaddr*>(&ReceiveAddress), &AddressLength);
-
-	if (Received < 0)
-	{
-		std::cerr << "recvfrom failed" << std::endl;
-		close(SockFd);
-		return 1;
-	}
-
-	//
-	// Extract timestamps from the received packet.
-	//
-	std::uint32_t TransmitTimeSeconds = (NtpPacket[40] << 24) | (NtpPacket[41] << 16) | (NtpPacket[42] << 8) | NtpPacket[43];
-
-	//
-	// Convert NTP time to UNIX time.
-	//
-	time_t UnixTime = static_cast<time_t>(TransmitTimeSeconds - NTP_TIMESTAMP_DELAT);
-
-	std::cout << "Synchronized time: " << ctime(&UnixTime);
-
-	close(SockFd);
 	return 0;
 }
--- Internet/NTPv3.hpp
+++ Internet/NTPv3.hpp
@@ -11,3 +11,23 @@ constexpr std::uint8_t VN_3 = 0b011;
 constexpr std::uint8_t MODE_CLIENT = 0b011;
 
 constexpr std::uint8_t NTP_HEADER_BYTE = (LI_NO_WARNING << 6) | (VN_3 << 3) | MODE_CLIENT;
+
+#pragma pack(push, 1)
+typedef struct _NTP_3_HEADER
+{
+	std::uint8_t LeapIndicator : 2;					// 2 bits
+	std::uint8_t VersionNumber : 3;					// 3 bits
+	std::uint8_t Mode : 3;							// 3 bits
+	std::uint8_t Stratum;							// 8 bits
+	std::int8_t PollInterval;						// 8 bits
+	std::int8_t Precision;							// 8 bits
+	std::int32_t RootDelay;							// 32 bits
+	std::int32_t RootDispersion;					// 32 bits
+	std::uint32_t ReferenceClockIdentifier;			// 32 bits
+	std::uint64_t ReferenceTimestamp;				// 64 bits
+	std::uint64_t OriginateTimestamp;				// 64 bits
+	std::uint64_t ReceiveTimestamp;					// 64 bits
+	std::uint64_t TransmitTimestamp;				// 64 bit
+
+} NTP_3_HEADER, *PNTP_3_HEADER;
+#pragma pack(pop)
