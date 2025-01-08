0181118c8f6579f1c31d566927dd4b450e2c5558
ConfederateCat <shmurkioskull@gmail.com>
2025-01-08 11:26:22 -0500

    changed

--- Internet/Entry.cpp
+++ Internet/Entry.cpp
@@ -1,18 +1,5 @@
-#include <iostream>
-#include <cstdint>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <unistd.h>
+#include "Internet.hpp"
 
-constexpr std::int32_t NTP_PORT = 123;
-constexpr std::uint64_t NTP_PACKET_SIZE = 48;
-constexpr std::uint32_t NTP_TIMESTAMP_DELAT = 2208988800;
-
-constexpr std::uint8_t LI_NO_WARNING = 0b00;
-constexpr std::uint8_t VN_3 = 0b011;
-constexpr std::uint8_t MODE_CLIENT = 0b011;
-
-constexpr std::uint8_t NTP_HEADER_BYTE = (LI_NO_WARNING << 6) | (VN_3 << 3) | MODE_CLIENT;
 
 int
 main(
